c1b3cc70b267d47621c4610d4679caeea668d2b4
RafaelSolier <rafael.solier@utec.edu.pe>
2025-04-26 18:57:18 -0500

    Complete  entidad Vehiculo

--- src/main/java/org/e2e/labe2e01/vehicle/domain/Vehicle.java
+++ src/main/java/org/e2e/labe2e01/vehicle/domain/Vehicle.java
@@ -1,7 +1,29 @@
 package org.e2e.labe2e01.vehicle.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Data
 @NoArgsConstructor
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    @Column(nullable = false)
+    private int fabrication_year;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false, unique = true)
+    private String license_plate;
+
+    @Column(nullable = false)
+    private String model;
 }
--- src/main/java/org/e2e/labe2e01/vehicle/infrastructure/VehicleRepository.java
+++ src/main/java/org/e2e/labe2e01/vehicle/infrastructure/VehicleRepository.java
@@ -1,7 +1,10 @@
 package org.e2e.labe2e01.vehicle.infrastructure;
 
 
+import org.e2e.labe2e01.vehicle.domain.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface VehicleRepository extends JpaRepository<> {
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 }
